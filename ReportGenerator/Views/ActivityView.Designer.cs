faadd39f0431ae63452b250ed31121a3be736c23
Evandro <evandrolrff@gmail.com>
2023-02-08 01:13:30 -0300

    correções na interface

--- ReportGenerator/Views/ActivityView.Designer.cs
+++ ReportGenerator/Views/ActivityView.Designer.cs
@@ -184,7 +184,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(757, 267);
+            this.tabPage2.Size = new System.Drawing.Size(698, 269);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Detalhes da atividade";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -294,6 +294,8 @@
             this.ClientSize = new System.Drawing.Size(706, 361);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "ActivityView";
             this.Text = "ActivityView";
             this.panel1.ResumeLayout(false);
